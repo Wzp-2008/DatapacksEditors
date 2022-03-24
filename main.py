56e6db9185ac970342c0fe47755f61416feb7140
qingchenyouforcc <qingchenformc@outlook.com>
2022-03-24 18:08:01 +0800

    feat:Add full_pack create and UI #7

--- main.py
+++ main.py
@@ -36,10 +36,41 @@ class MC_Version_Management_Window(QWidget, Ui_Form):
 class Create_Full_Window(QWidget, Ui_createfullwindows):
     def __init__(self):
         super(Create_Full_Window, self).__init__()
+        self.datapackPath = None
+        self.pPath = None
+        self.projectName = None
         self.setupUi(self)
+        # 改变数据包路径
+        self.btn_change_Pack.clicked.connect(self.choosePackPath)
+        self.datapackPathDialog = QFileDialog()
+        self.datapackPathDialogTitle = "选择路径"
+        # 获取项目有关信息
+        self.packname.textChanged.connect(self.getProjectName)
+        self.packPath.textChanged.connect(self.getPackPath)
+        self.projectPath.textChanged.connect(self.getProjectPath)
+        # 创建按钮连接
+        self.create_full_c.clicked.connect(self.create_full_pack)
+
+    def getProjectName(self, text):
+        self.projectName = text
+        print("projectName:" + str(text))
+
+    def getProjectPath(self, text):
+        self.pPath = text
+        print("projectPath:" + str(text))
+
+    def getPackPath(self, text):
+        self.datapackPath = text
+        print("datapackPath:" + str(text))
+
+    def choosePackPath(self):
+        self.datapackPath = self.datapackPathDialog.getOpenFileName(self, self.datapackPathDialogTitle)
 
     def OPEN(self):
         self.show()
+
+    def create_full_pack(self):
+        pass
 
 
 class DatapacksEditors(QMainWindow, Ui_MainWindow):
@@ -95,8 +126,8 @@ class DatapacksEditors(QMainWindow, Ui_MainWindow):
         # 文件树
         self.fileModel = QFileSystemModel()
         self.fileModel.setRootPath("C:/")
-        #self.fileTree.setModel(self.fileModel)
-        #self.fileTree.doubleClicked.connect(self.initUI)
+        # self.fileTree.setModel(self.fileModel)
+        # self.fileTree.doubleClicked.connect(self.initUI)
         # 打开文件
         self.fileDialogTitle = "打开文件"
         self.open_project.triggered.connect(self.On_open_project_btn_click)
